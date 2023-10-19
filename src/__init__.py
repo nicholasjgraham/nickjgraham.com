d41f86efd4c2d4fe2007635f50806d552921bafe
Nick Graham <ngraham1024@gmail.com>
2023-10-19 13:58:42 -0400

    Page size adjustment

--- src/__init__.py
+++ src/__init__.py
@@ -152,8 +152,8 @@ def pdf_gen():
         pdf_options = {
             'enable-local-file-access': True,
             'javascript-delay': '1000',
-            'page-width': '1100px',
-            'page-height': '3400px'
+            'page-width': '1000px',
+            'page-height': '3200px'
         }
         # Generate PDF file from HTML data
         pdf_data = pdfkit.from_string(resume_html, options=pdf_options)
