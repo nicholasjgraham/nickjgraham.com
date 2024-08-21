b7f43815b1ec11abd51ffe853f272c3e6d6e2aaf
Nick Graham <ngraham1024@gmail.com>
2024-08-20 22:17:13 -0400

    Fixed replace value

--- src/utils.py
+++ src/utils.py
@@ -18,7 +18,7 @@ def auto_link(data: str, link_list: dict) -> str:
     # Iterate through our dict object
     for key, value in link_list.items():
         # Replace the first instance of the dict key with the replacement value
-        data = data.replace(f" {key} ", f"<a href=\"{value}\">{key}</a>", 1)
-        data = data.replace(f" {key},", f"<a href=\"{value}\">{key}</a>", 1)
+        data = data.replace(f" {key} ", f" <a href=\"{value}\">{key}</a> ", 1)
+        data = data.replace(f" {key},", f" <a href=\"{value}\">{key}</a>,", 1)
     # Return the updated data
     return data
