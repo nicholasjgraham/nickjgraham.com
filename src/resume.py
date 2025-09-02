cb217bd551160a3a63b4b6dbb5bdd33128a7ba14
Nick Graham <ngraham1024@gmail.com>
2025-09-02 12:15:41 -0400

    Resume copy updates

--- src/resume.py
+++ src/resume.py
@@ -220,7 +220,9 @@ def get_resume():
                 'Created many CI/CD pipelines in Jenkins to turn complex processes like code builds, file deployments, or configuration changes into scheduled, or one-click operations. This greatly reduced the time and effort required to deploy changes, as well as reducing the chance for human error.',
                 'Automated the transition from CentOS to RHEL, and subsequent upgrades to RHEL 9. This reduced the amount of labor required to perform these upgrades by the whole engineering staff.',
                 'Organized and streamlined Ansible playbook usage, including the development of easy-to-use environment build scripts, and personal training/mentoring for other engineers. This enhanced the use of Ansible and improved automation capabilities for everyone\'s work across the team.',
-                'Created a custom internal site with Python that integrates with our SSO system APIs. This provides users across the company with a central place to find SSO links, rather than relying on links maintained in wikis or emails.'
+                'Created a custom internal site with Python that integrates with our SSO system APIs. This provides users across the company with a central place to find SSO links, rather than relying on links maintained in wikis or emails.',
+                'Supported development teams, providing infrastructure and security expertise to help make project decisions and clear roadblocks.'
+                'Was part of an on-call rotation, providing support at the highest technical level to resolve production issues.'
             ],
             'technologies': {
                 'Ansible',
@@ -245,9 +247,9 @@ def get_resume():
             'date': '2013 - 2016',
             'location': 'Albany, NY',
             'achievements': {
-                'Architected and implemented up all of the backup infrastructure required to support disaster recovery for all of Xerox\'s systems',
+                'Architected and implemented up all of the backup infrastructure required to support disaster recovery for all of the systems in my organization',
                 'Led VMware expansion to two new datacenters in China, including hardware purchasing, deployment, and configuration.',
-                'Created a VM deployment process automation from the ground up with VMware Orchestrator.',
+                'Created a self-service tool for employees to build sandbox environments and adjust the scale of production services',
                 'Enhanced the system lifecycle process around all of our VMware infrastructure. Everything from creation of new servers, patching existing ones, updating configurations, and server decommissioning was streamlined and automated in some way.'
             },
             'technologies': {
@@ -291,7 +293,6 @@ def get_resume():
             'Windows Server'
         ],
         'Programming': [
-            'Bash',
             'Javascript',
             'Powershell',
             'Python'
